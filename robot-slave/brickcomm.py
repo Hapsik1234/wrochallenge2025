1528fbc669b6c2704c09ea39a7a7f6e5459c3572
Bolesław Bondyra <bolek.bondyra@protonmail.com>
2025-05-26 16:05:16 +0200

    Refactor robot control code: improve motor handling, add tests, and update settings

--- robot-slave/brickcomm.py
+++ robot-slave/brickcomm.py
@@ -1,18 +1,15 @@
 from console import debug_print
-
-debug_print("ASDJFGSAUDHG")
 import rpyc # type: ignore
 from time import sleep
-
 
 
 conn = rpyc.classic.connect('192.168.0.2', port=42069)
 
 ev3dev2_motor = conn.modules['ev3dev2.motor']
 
-sensors_motor = ev3dev2_motor.LargeMotor(ev3dev2_motor.OUTPUT_B)
-gate_motor = ev3dev2_motor.LargeMotor(ev3dev2_motor.OUTPUT_A)
+brick_sensors_motor = ev3dev2_motor.MediumMotor(ev3dev2_motor.OUTPUT_B)
+brick_gate_motor = ev3dev2_motor.LargeMotor(ev3dev2_motor.OUTPUT_A)
 
-sensors_motor.run_timed(time_sp=1000, speed_sp=600)
+brick_sensors_motor.run_timed(time_sp=1000, speed_sp=600)
 
 # sensors_motor.turn_to_angle(speed=100, angle_target_degrees=60, error_margin=1)
--- robot-slave/main.py
+++ robot-slave/main.py
@@ -1,52 +1,48 @@
-# Wejścia:
-# Kostka master:
-# A -> Podnoszenie swastyki
-# B -> swastyka obracanie
-# C -> Silnik Lewy (od tyłu)
-# D -> Silnik Prawy (od tyłu)
-# Kostka slave:
-# A -> Klapa z tyłu
-# B -> color sensory
-
 import os
 import sys
 import time
 
-
-# from brickcomm import sensors_motor, gate_motor // do poszczególnych
+# from brickcomm import *  # deprecated
 from arduinocomm import *
 
 from storage import *
 from gate import *
 from sensors import *
-from mainMovingCompartment import mainCompartment as Steering
+from mainMovingCompartment import main_compartment as Steering
 from console import *
-
 
 
 # state constants
 ON = True
 OFF = False
 
-def main():
-    '''The main function of our program'''
 
-    # set the console just how we want it
+
+def main():
+    # --------------
+    # INIT
+    # --------------
+
     reset_console()
     set_cursor(OFF)
     set_font('Lat15-Terminus24x12')
-    # print something to the screen of the device
-    print('Hello World!')
 
-    # print something to the output panel in VS Code
+    print('Hello World!')
     debug_print('Hello VS Code!')
 
-    # wait a bit so you have time to look at the display before the program
-    
-    
-    time.sleep(5)
+    _storage = Storage()
+    _moving = main_compartment()
 
-    # connect()
+    _sensors = Sensors(brick_sensors_motor)
+    _gate = Gate(brick_gate_motor)
+
+    # --------------
+    # ACTUAL CODE
+    # --------------
+
+    _storage.lift(_storage.UP)
+    
+    
 
 if __name__ == '__main__':
     main()
